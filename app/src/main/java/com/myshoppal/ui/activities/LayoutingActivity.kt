8adc69a622ce33a445060191961ec684034654e9
nawfaldo <nawfaldo.fazli@gmail.com>
2021-09-15 09:53:38 +0700

    Login-Page App

---
+++ app/src/main/java/com/myshoppal/ui/activities/LayoutingActivity.kt
@@ -0,0 +1,12 @@
+package com.myshoppal.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myshoppal.R
+
+class LayoutingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layouting)
+    }
+}
